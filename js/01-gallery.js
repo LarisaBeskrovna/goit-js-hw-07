6bde134b101dfe42b839cba615a4e43700a22254
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-04-24 19:04:47 +0300

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+const liEl = itemContauner(galleryItems);
+list.insertAdjacentHTML('beforeend', liEl);
+
+function itemContauner(galleryItems) {
+    return galleryItems
+  .map(({original,preview, description}) => {
+     return `<li class="gallery__item"> 
+     <a class="gallery__link" href=${original}>
+     <img class="gallery__image" src=${preview} alt="${description}"></img> </a> </li>`
+  })
+    .join('');}
+
+
+
+console.log(itemContauner(galleryItems));
+
+
