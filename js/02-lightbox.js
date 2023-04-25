2948fff47efdf16dcf891a37a1f23c7e6a556219
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-04-25 18:40:29 +0300

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const list = document.querySelector('.gallery');
+let instance;
+list.insertAdjacentHTML('beforeend', itemContauner());
+
+function itemContauner() {
+  return galleryItems
+  .map(({original,preview, description}) => {
+     return `<li class="gallery__item"> 
+     <a class="gallery__link" href=${original}>
+     <img class="gallery__image" 
+     src=${preview} 
+     alt="${description}"></img> </a> </li>`
+  })
+    .join('');}
+
+
+  const lightbox = new SimpleLightbox('.gallery__link', {});
+list.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  
+  instance.show();
+  document.addEventListener('keydown', closeModal);
+}
+
+function closeModal(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+    document.removeEventListener('keydown', closeModal);
+  }
+}
+
+list.addEventListener('click', onClick);
+
 
 console.log(galleryItems);
+
